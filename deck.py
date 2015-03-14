fc943e3c969e2ee431552c77f1d6d41002537ce4
Andrew <werdnaonaps@gmail.com>
2015-03-14 04:32:37 -0400

    created module for deck and card functionality

--- deck.py
+++ deck.py
@@ -1,6 +1,29 @@
 """Module for defining deck data"""
+class Card:
+    """class for defining card"""
+    suit = 0
+    rank = 0
+    VALUES = {
+            1: 'Ace',
+            11: 'Jack',
+            12: 'Queen',
+            13: 'King',
+            }
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def get_rank(self):
+        return self.rank
+    def get_card_name(self):
+        if self.get_rank in self.VALUES:
+            return self.VALUES[self.rank]
+        else:
+            return '' + self.get_rank
+
 class Deck:
     """Object for representing deck of 52 cards"""
+    deckList = []
     def __init__(self):
-        for i in range(0, 52):
-            self.list = i
+        for i in range(0, 4):
+            for j in range(0, 52):
+               self.deckList[j] = Card(i, j)
