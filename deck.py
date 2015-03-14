956968371f8c41de19d92e9945fb1bf80f84e2e6
Andrew <werdnaonaps@gmail.com>
2015-03-14 05:22:35 -0400

    created simple dealing functionality and shuffling

--- deck.py
+++ deck.py
@@ -1,7 +1,8 @@
+from random import shuffle
 """Module for defining deck data"""
 class Card:
     """class for defining card"""
-    suit = 0
+    #suit = 0
     rank = 0
     VALUES = {
             1: 'Ace',
@@ -9,21 +10,36 @@ class Card:
             12: 'Queen',
             13: 'King',
             }
-    def __init__(self, suit, rank):
-        self.suit = suit
+    def __init__(self, rank):
         self.rank = rank
     def get_rank(self):
         return self.rank
     def get_card_name(self):
-        if self.get_rank in self.VALUES:
+        if self.get_rank() in self.VALUES:
             return self.VALUES[self.rank]
         else:
-            return '' + self.get_rank
+            return '' + str(self.get_rank())
 
 class Deck:
     """Object for representing deck of 52 cards"""
     deckList = []
     def __init__(self):
         for i in range(0, 4):
-            for j in range(0, 52):
-               self.deckList[j] = Card(i, j)
+            for j in range(1, 14):
+                self.deckList.append(Card(j))
+    @classmethod
+    def shuffle(self):
+        """TODO: Docstring for shuffle.
+
+        :1: TODO
+        :returns: TODO
+
+        """
+        shuffle(self.deckList)
+    @classmethod
+    def pull_from_deck(self):
+        """TODO: Docstring for pull_from_deck.
+        :returns: TODO
+
+        """
+        return self.deckList.pop(0)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def main():
-    print "Game initialized"
-if __name__ == "__main__":
-    main()
-
+from deck import Deck
 def deal():
     """Deal the next card"""
     deck = Deck()
+    deck.shuffle()
+    print deck.pull_from_deck().get_card_name()
+
+def main():
+    print "Game initialized"
+    deal()
+if __name__ == "__main__":
+    main()
+
+
